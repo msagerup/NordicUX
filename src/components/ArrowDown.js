df61e5cec033e6b317522254fca4691650cfaac5
Magnus Sagerup <magnus.sagerup@gmail.com>
2018-01-21 16:52:42 +0100

    Added Home component, sentered Hero with flexbox and added arrow and contact icon component

---
+++ src/components/ArrowDown.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ArrowDown = () => (
+   <div id="arrow-down">Arrow</div>
+
+);
+
+export default ArrowDown;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import ArrowDown from './ArrowDown';
+import ContactIcon from './ContactIcon';
 import { NavLink } from 'react-router-dom';
 
 const Home = () => (
-  <div>
-    THIS IS HOME
-    
-  
+   <div>
+      <section className="hero">
+         <div className="hero__background-image"></div>
+         <div className="hero__content-text">
+            <h1 className="hero__title">Nordic UX Studio</h1>
+         </div>
+      </section>
+         <div id="home-right-side">
+            <ContactIcon />
+            <ArrowDown />
+         </div>  
    </div>
+
 );
 
 export default Home;

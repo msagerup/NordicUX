1db7c11d5d506a2037cfa496105400416a843151
Magnus Sagerup <magnus.sagerup@gmail.com>
2018-01-28 21:40:22 +0100

    practise

--- src/components/Golden.js
+++ src/components/Golden.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ContentLeft from '../components/ContentLeft';
 import ContentRight from '../components/ContentRight';
 
+
 const Golden = () => (
    <div className="golden full">
       <ContentLeft />     

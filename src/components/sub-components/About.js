2c9d54b05947ce8df92c7b249e0f7224032efe93
Magnus Sagerup <magnus.sagerup@gmail.com>
2018-02-21 20:14:18 +0100

    first pass on about, (not happy) setting up order tab, would need to implement state and sub components

--- src/components/sub-components/About.js
+++ src/components/sub-components/About.js
@@ -1,13 +1,43 @@
 import React from 'react';
 
-const About = () => {
+class About extends React.Component {
+   
+
+   age() {
+      let currentDate = new Date();
+      let year = currentDate.getFullYear();
+      const myDoB = 1981;
+      let myAge = year - myDoB;
+      return myAge
+   }
+
+   render () {
    return (
       <div className="content-holder large">
          <div className="infocorner content-holder__menu-tag">
             <h4>Abo-ut me</h4>
-         </div>   
+         </div>
+         <div className="about-content">
+            <h3>Well, meet.. me!</h3><hr/>
+            <p><span>Magnus Sagerup</span> is my name, I am {this.age()} years old and  and I am a self tought front-end
+            developer. I started learning in November 2016 as I realized that I realy wanted to follow my passion and 
+            truly learn how to code.</p>
+            <p>During my time learning how to code I have come to realize that alltough learning my yourself is truly a 
+            big challange, but it also enables you to keep up with the most up to date technologies. When I frist started I 
+            did not know about alot of these, just thinking about how much there was to learn keept me up at night, but well, hell,
+            look at me now, this whole page is built with React :)</p>
+            <h3>
+               What I know
+            </h3>
+               <hr/>
+            <p>I will focus on the MERN stack and continue developing with this stack on future projects.</p>
+            <div className="about-content__tech-images">
+               <img src="https://hevodata.com/blog/wp-content/uploads/2017/09/mn.png" alt="Node and Mongo DB logo"/>
+            </div>
+         </div>
       </div>
    )
+   }
 };
 
 export default About;
--- src/components/sub-components/Order.js
+++ src/components/sub-components/Order.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-const Order = () => {
+class Order extends React.Component {
+   render () {
+
    return (
       <div className="content-holder large">
          <div className="infocorner content-holder__menu-tag">
             <h4>Ord-er</h4>
          </div>
+         <div className="order-content">
+            <h3>I would like to work with you</h3>
+            <p>Please select what features you need for your business and I will get
+            in touch with you. Your selected options will be show at the bottom on this tab</p>
+         </div>
+         <div className="order-content__main-feature">
+
+            <ul>
+               <li>Item1</li>
+               <li>Item2</li>
+               <li>Item3</li>
+               <li>Item4</li>
+               <li>Item5</li>
+            </ul>
+
+         </div>
       </div>
    )
+   }
 };
 
 export default Order;
